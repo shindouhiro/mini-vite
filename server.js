9ecafda721075cade9d94e291274d1d6617ff624
Hikaru <shindouhiro@gmail.com>
2025-05-24 14:36:53 +0800

    feat: 添加样式文件引用和额外脚本
    - 在 index.html 中添加了 style.css 的链接
    - 在 index.html 中添加了 a.js 的脚本引用
    - 更新 server.js 以支持 CSS 文件的请求
    - 修改 main.js 中的 alert 为 console.log
    - 在 style.css 中添加了背景颜色样式

--- server.js
+++ server.js
@@ -2,18 +2,30 @@ import Koa from 'koa'
 import fs from 'fs'
 import path from 'path'
 const app = new Koa()
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+console.log(__dirname,path.dirname)
+console.log(new URL(import.meta.url).pathname)
 
 
 app.use(async (ctx) => {
-  const { url } = ctx.request
+  // ctx.body = 'Hello World'
+    const { url } = ctx.request
   if (url === '/') {
     ctx.type = "text/html"
-    ctx.body = fs.readFileSync("./index.html", "utf8")
-  } else if (url.endsWith('.js')) {
-    const p = path.join(__dirname, url)
-    console.log(p)
-    ctx.type = "application/javascript"
-    ctx.body = fs.readFileSync(p, 'utf8')
+    ctx.body = fs.readFileSync("./index.html","utf8")
+  } else if (url.endsWith('.js')) {// 
+    const p = path.join(__dirname, url)// 
+    console.log({// 
+      __dirname,// 
+      p,
+      // 
+      url// 
+    })// 
+    ctx.type = "application/javascript"// 
+    ctx.body = fs.readFileSync(p, 'utf8')// 
+  }else if (url.endsWith('.css')) {// 
+    ctx.type = "text/css"// 
+    ctx.body = fs.readFileSync(`.${url}`, 'utf8')// 
   }
 })
 
